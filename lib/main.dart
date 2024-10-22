3d569664d1a7af2ee71cbae14788c1956cc360d4
IT21185298 <it21185298@my.sliit.lk>
2024-10-22 16:11:12 +0530

    add to multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:ecommerce_app/statemanagement/favouriteProvider.dart';
+import 'package:ecommerce_app/statemanagement/provider/cart_model.dart';
 import 'package:ecommerce_app/views/firstPage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -8,16 +9,20 @@ import 'package:provider/provider.dart';
 void main() => runApp(
       DevicePreview(
         enabled: !kReleaseMode,
-        builder: (context) => const MyApp(), // Wrap your app
+        builder: (context) => const MyApp(),
       ),
     );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => FavoriteProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => FavoriteProvider()),
+        ChangeNotifierProvider(create: (context) => CartModel()),
+      ],
       child: const MaterialApp(
         useInheritedMediaQuery: true,
         builder: DevicePreview.appBuilder,
