f87165b8dd764bf12b3d4059e9bec2dcdf9a02b9
IT21185298 <it21185298@my.sliit.lk>
2024-10-22 10:10:09 +0530

    add to column

--- lib/views/mycart.dart
+++ lib/views/mycart.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerce_app/views/checkoutPage.dart';
+import 'package:ecommerce_app/widgets/customButton.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyCartPage extends StatefulWidget {
   final String? image;
@@ -29,179 +32,181 @@ class _MyCartPageState extends State<MyCartPage> {
         : '\$0.00'; // Default value if price is null
 
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back),
-        ),
-        title: const Text(
-          'My Cart',
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w900,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+          ),
+          title: const Text(
+            'My Cart',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w900,
+            ),
           ),
         ),
-      ),
-      body: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            color: const Color(0xFFF1F4FB),
-            width: 354,
-            height: 146,
-            margin: const EdgeInsets.only(left: 30, right: 30),
-            child: Row(
-              children: [
-                Container(
-                  width: 87,
-                  height: 77,
-                  margin: const EdgeInsets.only(
-                    left: 21,
-                    top: 21,
-                    bottom: 47,
-                  ),
-                  child: Image.network(
-                    widget.image ??
-                        'https://via.placeholder.com/150', // Using the passed image
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, left: 13),
-                      child: Expanded(
-                        child: Text(
-                          widget.title ??
-                              'Product Title', // Using the passed title
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w900,
-                            color: Color(0xFF6342E8),
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 8, left: 13),
-                      child: Text(
-                        'GEETA COLLECTION',
-                        style: TextStyle(
-                          fontSize: 9,
-                          fontWeight: FontWeight.w900,
-                          color: Color(0xFFA1A1A1),
-                        ),
-                      ),
-                    ),
-                    Row(
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 1, // Assuming one item in cart for now
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    color: const Color(0xFFF1F4FB),
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text(
-                            formattedPrice,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w900,
-                            ),
+                        Container(
+                          width: 87,
+                          height: 87,
+                          margin: const EdgeInsets.all(10),
+                          child: Image.network(
+                            widget.image ?? 'https://via.placeholder.com/150',
+                            fit: BoxFit.cover,
                           ),
                         ),
-                        const SizedBox(width: 2),
-                        const Text(
-                          'USD',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: 115,
-                  height: 146,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 5,
-                          left: 60,
-                        ),
-                        child: IconButton(
-                          onPressed: () {
-                            // Handle item removal
-                          },
-                          icon: const Icon(Icons.cancel_presentation_rounded),
-                        ),
-                      ),
-                      Container(
-                        color: const Color(0xFFD3D3D3),
-                        width: 82,
-                        height: 25,
-                        margin: const EdgeInsets.only(
-                          top: 68,
-                        ),
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: 27.33,
-                              height: 25,
-                              child: IconButton(
-                                onPressed: () {
-                                  if (quantity > 1) {
-                                    setState(() {
-                                      quantity--; // Decrease quantity
-                                    });
-                                  }
-                                },
-                                icon: const Icon(Icons.remove),
-                                padding: EdgeInsets.zero,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 27.33,
-                              height: 25,
-                              child: Center(
+                        Expanded(
+                          flex: 3,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 20),
                                 child: Text(
-                                  quantity.toString(),
+                                  widget.title ?? 'Product Title',
                                   style: const TextStyle(
-                                    fontSize: 18,
+                                    fontSize: 14,
                                     fontWeight: FontWeight.w900,
+                                    color: Color(0xFF6342E8),
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(top: 8),
+                                child: Text(
+                                  'GEETA COLLECTION',
+                                  style: TextStyle(
+                                    fontSize: 9,
+                                    fontWeight: FontWeight.w900,
+                                    color: Color(0xFFA1A1A1),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              width: 27.33,
-                              height: 25,
-                              child: IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    quantity++; // Increase quantity
-                                  });
-                                },
-                                icon: const Icon(Icons.add),
-                                padding: EdgeInsets.zero,
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      formattedPrice,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w900,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 2),
+                                    const Text(
+                                      'USD',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w900,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                        SizedBox(
+                          width: 110,
+                          height: 146,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 5, right: 10),
+                                child: Align(
+                                  alignment: Alignment.topRight,
+                                  child: IconButton(
+                                    onPressed: () {
+                                      // Handle item removal
+                                    },
+                                    icon: const Icon(
+                                        Icons.cancel_presentation_rounded),
+                                  ),
+                                ),
+                              ),
+                              const Spacer(),
+                              Container(
+                                color: const Color(0xFFD3D3D3),
+                                width: 80,
+                                height: 25,
+                                margin: const EdgeInsets.only(bottom: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        if (quantity > 1) {
+                                          setState(() {
+                                            quantity--; // Decrease quantity
+                                          });
+                                        }
+                                      },
+                                      icon: const Icon(Icons.remove),
+                                      padding: EdgeInsets.zero,
+                                      constraints: const BoxConstraints(),
+                                    ),
+                                    Text(
+                                      quantity.toString(),
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w900,
+                                      ),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          quantity++; // Increase quantity
+                                        });
+                                      },
+                                      icon: const Icon(Icons.add),
+                                      padding: EdgeInsets.zero,
+                                      constraints: const BoxConstraints(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
-          );
-        },
-      ),
-    );
+            CustomButton(
+              buttonText: "GO TO CHECKOUT",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CheckOutPage(),
+                  ),
+                );
+              },
+            )
+          ],
+        ));
   }
 }
